cb6f6f6fd16817ffa48130686cdd439496889c40
apnidukan3107 <dabhinavghan71@gmail.com>
2026-08-01 23:59:09 +0530

    Add service worker registration
    Register service worker on window load.

--- src/main.jsx
+++ src/main.jsx
@@ -6,4 +6,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
